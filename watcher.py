b94c46af3e8a9190f02772617b8505be85a292e3
Andreetje <andre@jeanmaire.nl>
2021-03-12 11:20:57 +0100

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -24,7 +24,7 @@ while True:
       remotePath = remoteMount + "/" + name + ext
       
       try:
-        print("Move Scan: '" + file + "' -> '" + remotePath + "'")
+        print("Move File: '" + file + "' -> '" + remotePath + "'")
         shutil.copyfile(file, remotePath)
         os.remove(file)
       except (FileNotFoundError, OSError) as err:
